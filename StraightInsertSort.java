51b35977a97ccad92ec2bc42cf853a25b5d56ecc
werdedeage <werdedeage@163.com>
2016-04-08 18:52:13 +0800

    Update and rename DirectInsertSort.java to StraightInsertSort.java

--- StraightInsertSort.java
+++ StraightInsertSort.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
  * @since 2016-04-08
  * @author werdedeage
  */
-public class DirectInsertSort {
+public class StraightInsertSort {
 	public static void main(String[] args) {
 		int[] a = {21, 2, 9, 72, 28, 90, 52, 8, 9, 3};
 		System.out.println("排序前：\n" + Arrays.toString(a));
-		directInsertSort(a);
+		straightInsertSort(a);
 		System.out.println("排序后：\n" + Arrays.toString(a));
 	}
 
-	private static void directInsertSort(int[] a) {
+	private static void straightInsertSort(int[] a) {
 		for (int i = 1; i < a.length; i++) {
 			int temp = a[i]; //要插入的值
 			int j;
