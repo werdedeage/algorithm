8b5350d42dca8c9ac2e2b8eb840623370fee058c
werdedeage <werdedeage@163.com>
2016-04-08 22:24:13 +0800

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -9,16 +9,21 @@ public class QuickSort {
   public static void main(String[] args) {
 		int[] a = {21, 2, 9, 72, 28, 90, 52, 8, 9, 3};
 		System.out.println("排序前：\n" + Arrays.toString(a));
-		quickSort(a, 0, a.length - 1);
+		quickSort(a);
 		System.out.println("排序后：\n" + Arrays.toString(a));
 	}
 
   	//快速排序
-	private static void quickSort(int[] a, int low, int high) {
+	private static void quickSort(int[] a) {
+		quickSortPart(a, 0, a.length-1);
+	}
+	
+	//部分快速排序
+	private static void quickSortPart(int[] a, int low, int high) {
 		if(low < high){
 			int middle = split(a, low, high);
-			quickSort(a, low, middle - 1);
-			quickSort(a, middle + 1, high);
+			quickSortPart(a, low, middle - 1);
+			quickSortPart(a, middle + 1, high);
 		}
 	}
 	
